f038c12ac496a4b575ca8176145d270af1e62006
chals <fison67@nate.com>
2019-08-18 19:05:25 +0900

    Update kt-connector.groovy

--- smartapps/fison67/kt-connector.src/kt-connector.groovy
+++ smartapps/fison67/kt-connector.src/kt-connector.groovy
@@ -134,7 +134,7 @@ def initialize() {
     state["messageIndex"] = 0;
     
     unschedule()
-    schedule("0 */1 * * * ?", takeTokenAuto)
+    schedule("0 */60 * * * ?", takeTokenAuto)
 }
 
 def takeTokenAuto(){
